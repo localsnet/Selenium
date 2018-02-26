e7b7dd82b89389610d13fbd40712ac7eadca91b8
localsnet <localsnet@gmail.com>
2018-02-26 17:04:05 +0300

    FirstIDEATest

---
+++ src/test/java/com/gmail/email/FirstTest.java
@@ -0,0 +1,42 @@
+package com.gmail.email;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+    private static ChromeDriver driver;
+    WebElement element;
+
+    @BeforeClass
+    public static void openBrowser(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test // Marking this method as part of the test
+    public void gotoSeleniumWikiPage() {
+        // Go to the Wikipedia home page
+        driver.get("https://en.wikipedia.org/");
+        // Find the text input element by its id and type "Selenium"
+        driver.findElement(By.id("searchInput")).sendKeys("Selenium");
+        // Click search button
+        driver.findElement(By.id("searchButton")).click();
+        // Get text from header of the Selenium page
+        String header = driver.findElement(By.id("firstHeading")).getText();
+        // Verify that header equals "Selenium"
+        Assert.assertEquals(header, "Selenium");
+
+    }
+
+    @AfterClass
+    public static void closeBrowser(){
+        driver.quit();
+    }
+}
